793e7593cef738ec10eaf420044c1e5b5d6b5eed
fianoo <fabiomiguel98@hotmail.com>
2018-04-09 15:41:57 +0100

    inicio do Servidor
    massPrinter();
    digitChecker();

--- SUPServidor.c
+++ SUPServidor.c
@@ -1,1 +1,35 @@
-
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+bool digitChecker(char n[150]){
+  int i=0;
+  while(n[i]!='\0'){
+    if(!isdigit(n[i]))
+      return false;
+    i++;
+  }
+  return true;
+}
+void massPrinter(char n[100][100]){
+  int i=0,j=0;
+  while(n[i][j]!='\0'){
+    printf("%d) ",i+1);
+    while(n[i][j]!='\0'){
+      printf("%c",n[i][j]);
+      j++;
+    }
+    printf("\n");
+    j=0;
+    i++;
+  }
+  }
+void main(){
+  char menuOption[100][100]={"Login","Registo de um novo user","Sair"};
+  char optionMenu[150];
+  scanf("%s", optionMenu);
+  if(!digitChecker(optionMenu)) return;
+  massPrinter(menuOption);
+  
+  
+  
+}
