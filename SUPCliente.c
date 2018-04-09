da68601c730441a17c26a18dc8beac5f06220eb4
Marcobarros10 <38216737+Marcobarros10@users.noreply.github.com>
2018-04-09 13:45:33 +0000

    inicio do login

--- SUPCliente.c
+++ SUPCliente.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int main() {
+  char nome[10], pass[10];
+  int a;
+  scanf("%d", &a);
+  if(a == 1)
+    {
+      printf("autenticação/login\n");
+      printf("nome do utilizador\n");
+      scanf("%s", &nome);
+      printf("password\n");
+      scanf("%s ", &pass);
+    }
+  return 0;
+}
 
